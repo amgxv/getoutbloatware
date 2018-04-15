a535a026d6872904d79073330654839a3edc1fe0
amgxv <32461531+amgxv@users.noreply.github.com>
2018-04-15 16:11:57 +0200

    Update LimpiarWin10.ps1

--- LimpiarWin10.ps1
+++ LimpiarWin10.ps1
@@ -1,4 +1,7 @@
-
+# 3D Builder
+get-appxpackage Microsoft.Office.3DBuilder | remove-appxpackage
+# 3D Viewer
+get-appxpackage Microsoft.Office.Microsoft3DViewer | remove-appxpackage
 # Alarms & Clocks: 
 get-appxpackage Microsoft.WindowsAlarms | remove-appxpackage
 # Calculator: 
@@ -11,14 +14,24 @@ get-appxpackage Microsoft.ZuneMusic | remove-appxpackage
 get-appxpackage Microsoft.windowscommunicationsapps | remove-appxpackage
 # Maps: 
 get-appxpackage Microsoft.WindowsMaps | remove-appxpackage
+# Messaging :
+get-appxpackage Microsoft.Office.Messaging | remove-appxpackage
 # Movies & Tv: 
 get-appxpackage Microsoft.ZuneVideo | remove-appxpackage
 # OneNote: 
 get-appxpackage Microsoft.Office.OneNote | remove-appxpackage
+# Paint3D:
+get-appxpackage Microsoft.Office.MSPaint | remove-appxpackage
 # People: 
 get-appxpackage Microsoft.People | remove-appxpackage
+# Print3D:
+get-appxpackage Microsoft.Office.MSPrint | remove-appxpackage
 # Photos: 
 get-appxpackage Microsoft.Windows.Photos | remove-appxpackage
+# Solitaire Collection
+get-appxpackage Microsoft.Office.MicrosoftSolitaireCollection | remove-appxpackage
+# Skype (Win10)
+get-appxpackage Microsoft.Office.SkypeApp | remove-appxpackage
 # Voice Recorder: 
 get-appxpackage Microsoft.WindowsSoundRecorder | remove-appxpackage
 # Xbox: 
